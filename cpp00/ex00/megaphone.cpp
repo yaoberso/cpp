89324a336d38d9b2cc4a68cf903fa9c12bbcc39f
VotreNom <VotreEmail>
2025-09-18 15:26:18 +0200

    ajout fichier txt note video intra

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 {
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOIS *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOIS *\n";
 	}
 	else
 	{
